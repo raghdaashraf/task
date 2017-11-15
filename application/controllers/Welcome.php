3cbe79f8f29196797672e1339dc3b72db9360d9b
raghdaashraf <33671491+raghdaashraf@users.noreply.github.com>
2017-11-15 22:22:50 +0200

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,72 +11,53 @@ class Welcome extends CI_Controller {
     }
 
     public function index() {
-
-//    $curlSession = curl_init();
-//    curl_setopt($curlSession, CURLOPT_URL, 'https://api.myjson.com/bins/tl0bp');
-//    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-//    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-//
-//    $jsonData = (curl_exec($curlSession));
-//    curl_close($curlSession);
-//  print_r( json_decode ($jsonData,true));die;
-//print_r( $json);
-
-
         if ($this->input->post()) {
             $hotelName = trim($this->input->post('hotelName', TRUE));
             $city = trim($this->input->post('city', TRUE));
             $price = trim($this->input->post('price', TRUE));
             $dateFrom = trim($this->input->post('dateFrom', TRUE));
             $dateTo = trim($this->input->post('dateTo', TRUE));
-            $json = (array) json_decode(file_get_contents('https://api.myjson.com/bins/tl0bp'), true);
 
-//print_r($json);die;
-//foreach($json as $value)
-//{
-//
-//    $array_city[] = $value['hotels'];	
-//
-//}
-//print_r($array_city);die;
-
-
+            $json = $this->getArray('https://api.myjson.com/bins/tl0bp');
 
             $match = false;
             $newArray = array();
-            foreach ($json as $key => $value) {//print_r($value);
+            $newArray2 = array();
+            foreach ($json as $key => $value) {
                 foreach ($value as $key2 => $value2) {
+
                     if ($value2['city'] == $city) {
 
                         if (!in_array($newArray, $value[$key2])) {
-                            (array_push($newArray, $value[$key2]));
+                            //$newArray['city']=$value2['city'];
+                            array_push($newArray, $value[$key2]);
                         }
                     }
+
+
+
                     if ($value2['name'] == $hotelName) {
 
                         if (!in_array($newArray, $value[$key2])) {
-                            (array_push($newArray, $value[$key2]));
+
+                            //$newArray['name']=$value2['name'];
+                            array_push($newArray, $value[$key2]);
                         }
                     }
+
+
 
                     if ($value2['price'] == $price) {
 
                         if (!in_array($newArray, $value[$key2])) {
-                            (array_push($newArray, $value[$key2]));
+                            //  $newArray['price']=$value2['price'];
+                            array_push($newArray, $value[$key2]);
                         }
                     }
 
-
                     foreach ($value2['availability'] as $key3 => $value3) {
 
-                        if ($value3['from'] == $dateFrom) {
-
-                            if (!in_array($newArray, $value[$key3])) {
-                                (array_push($newArray, $value[$key3]));
-                            }
-                        }
-
-                        if ($value3['to'] == $dateTo) {
+                        if ((date($value3['from']) >= date($dateFrom)) && (date($value3['to']) <= date($dateTo))) {
 
                             if (!in_array($newArray, $value[$key3])) {
                                 (array_push($newArray, $value[$key3]));
@@ -84,16 +65,10 @@ class Welcome extends CI_Controller {
                         }
                     }
                 }
-            } //print_r($newArray);
-            //die;
-foreach($newArray as $key=>$value)
-{
-   if($value['name']==$hotelName && $value['city']==$city) 
-   {
-       echo "kk";
-   }
-}die;
-            $data['result'] = $hotelName;
+            }
+
+
+            $data['result'] = $newArray;
             $this->load->view('header', $data);
             $this->load->view('search');
             $this->load->view('footer');
@@ -103,6 +78,23 @@ foreach($newArray as $key=>$value)
             $this->load->view('search');
             $this->load->view('footer');
         }
+    }
+
+    /**
+     * 
+     * @param type $url for file json api
+     * @return array data of json
+     */
+    private function getArray($url) {
+        $curlSession = curl_init();
+        curl_setopt($curlSession, CURLOPT_URL, $url);
+        curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+
+        $jsonData = (curl_exec($curlSession));
+        curl_close($curlSession);
+        return ((array) ( json_decode($jsonData, true)));
+        //$json = (array) json_decode(file_get_contents('https://api.myjson.com/bins/tl0bp'), true);
     }
 
 }
