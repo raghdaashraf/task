c1e2f9bca7e00ad566fc0e14d00c03e774f057af
raghdaashraf <33671491+raghdaashraf@users.noreply.github.com>
2017-11-15 22:23:39 +0200

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -47,77 +47,7 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Organization<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="<?php echo site_url('company.php');?>">Companies</a></li>
-            <li><a href="#">Branches</a></li>
-            <li><a href="#">Departments</a></li>
-            <li><a href="<?php echo site_url('employee/employeeSearch');?>">Employees</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Policy</a></li>
-            <li><a href="#">Announcements</a></li>
-            <li><a href="#">News</a></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Employees<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="<?php echo site_url('permission.php');?>">Permissions</a></li>
-            <li><a href="<?php echo site_url('memo.php');?>">Memos</a></li>
-            <li><a href="<?php echo site_url('mission.php');?>">Missions</a></li>
-            <li><a href="<?php echo site_url('transfere.php');?>">Transfers</a></li>
-            <li><a href="<?php echo site_url('overtime.php');?>">Overtime</a></li>
-            <li><a href="<?php echo site_url('evaluation.php');?>">Evaluations</a></li>
-            <li><a href="<?php echo site_url('complain.php');?>">Complaints</a></li>
-            <li><a href="<?php echo site_url('warning.php');?>">Warnings</a></li>
-            <li><a href="<?php echo site_url('promotion.php');?>">Promotions</a></li>
-            <li><a href="<?php echo site_url('survey.php');?>">Survey</a></li>
-            <li><a href="<?php echo site_url('resignation.php');?>">Resignations</a></li>
-            
-          </ul>
-        </li>
-        
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Work Details<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="<?php echo site_url('attendance.php');?>">Attendance</a></li>
-            <li><a href="<?php echo site_url('vacation_list.php');?>">Vacations</a></li>
-            <li><a href="<?php echo site_url('holiday.php');?>">Holidays</a></li>
-            <li><a href="<?php echo site_url('hours.php');?>">Employee Hours</a></li>
-            <li><a href="<?php echo site_url('worksheet.php');?>">Worksheet</a></li>
-          </ul>
-        </li>
-        
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Payrolls<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          
-            <li><a href="<?php echo site_url('salary.php');?>">Salaries</a></li>
-            <li><a href="<?php echo site_url('overtime.php');?>">Overtimes</a></li>
-            <li><a href="<?php echo site_url('deduction.php');?>">Deductions</a></li>
-            <li><a href="<?php echo site_url('bonus.php');?>">Bonuses</a></li>
-
-          </ul>
-        </li>
-        
-        <li><a href="<?php echo site_url('training.php');?>">Training</a></li>
-        
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Recruitment<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          
-            <li><a href="#">Job Posts</a></li>
-            <li><a href="#">Job Candidates</a></li>
-            <li><a href="#">Job Interviews</a></li>
-            <li><a href="#">Job Tests</a></li>
-
-          </ul>
-        </li>
-      
-      </ul>
-      
+    
       
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -126,4 +56,3 @@
 
     <div id="wrapper">
         <div class="overlay"></div>
-    
